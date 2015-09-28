19c5b3d2957cb77a995d46634599408e9167fe9e
Q <Qrules@users.noreply.github.com>
2015-09-28 15:45:16 -0500

    Create arrayListProject.java

---
+++ arrayListProject.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class arrayListProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		List<String> list = new ArrayList<String>(); //Initializes the array list
+		list.add("Hello World"); //Adds an element to the list
+		System.out.println(list.get(0)); //Gets the first element from the list and prints it
+	}
+}
